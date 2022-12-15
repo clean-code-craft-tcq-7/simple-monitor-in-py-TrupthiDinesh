ca158fca191ea0dece7bd87218987d5f22efe52a
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-15 09:51:36 +0530

    Update Battery_Status.py

--- Battery_Status.py
+++ Battery_Status.py
@@ -1,10 +1,10 @@
 from SOC import check_soc
-from Temperature import check_temperature
+from Temperature import Check_Temperature
 from Charge_rate import check_charge_rate
 
 
 def battery_is_ok(temperature, soc, charge_rate):
-  temperature_check = check_temperature(temperature)
+  temperature_check = Check_Temperature(temperature)
   soc_check = check_soc(soc)
   charge_rate_check = check_charge_rate(charge_rate)
   return (temperature_check and soc_check and charge_rate_check)
