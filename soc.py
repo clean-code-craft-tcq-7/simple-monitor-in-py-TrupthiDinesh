9a39ba5bef3d2ed83165b5e7a171b277a0381b7a
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-15 09:59:01 +0530

    Update soc.py

--- soc.py
+++ soc.py
@@ -1,6 +1,6 @@
 def check_soc(soc):
-   if soc < 20 or soc > 80:
-        print("'State of Charge is out of range!")
+    if soc < 20 or soc > 80:
+        print("State of charge of battery is not OK!")
         return False
-    print("State of charge of battery is ok")
+    print("State of charge of battery is OK!")
     return True
