435562b5459982c61cd6e99167f6f1a7a9fc2ead
TrupthiDinesh <115698754+TrupthiDinesh@users.noreply.github.com>
2022-12-15 10:02:06 +0530

    Update charge_rate.py

--- charge_rate.py
+++ charge_rate.py
@@ -1,6 +1,7 @@
+
 def check_charge_rate(charge_rate):
-  if charge_rate > 0.8:
-        print("Charge rate is out of range!")
+    if charge_rate > 0.8:
+        print("Charge rate of battery is not OK!")
         return False
-   print("Charge rate of battery is ok!")
-   return True
+    print("Charge rate of battery is OK!")
+    return True
